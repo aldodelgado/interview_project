8ed979624c31c218d363ece79e51bf6bdbe15d5b
Aldo Delgado <aldo@osstek.com>
2024-11-07 17:17:19 -0500

    Generate Book model with title, author, publication_year, and isbn attributes

---
+++ test/models/book_test.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id               :bigint           not null, primary key
+#  author           :string
+#  isbn             :string
+#  publication_year :integer
+#  title            :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+require "test_helper"
+
+class BookTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
