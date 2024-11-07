e69b1b8e6df2f7513ea66b1507104e7059c95a84
Aldo Delgado <aldo@osstek.com>
2024-11-07 17:19:11 -0500

    Generate Review model with user and book references, rating, and content attributes

---
+++ test/models/review_test.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  rating     :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  book_id    :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_reviews_on_book_id  (book_id)
+#  index_reviews_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (book_id => books.id)
+#  fk_rails_...  (user_id => users.id)
+#
+require "test_helper"
+
+class ReviewTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
