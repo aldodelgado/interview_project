4cefec2edb1c085b6942409c0442148d48b25313
Aldo Delgado <aldo@osstek.com>
2024-11-08 12:09:29 -0500

    Handle expired session with redirect to login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  rescue_from ActionController::InvalidAuthenticityToken do
+    redirect_to new_user_session_path, alert: "Your session has expired. Please log in again."
+  end
 end
